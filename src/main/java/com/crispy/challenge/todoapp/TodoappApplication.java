f0c5759828ef0f5d8dbd6f31173801a9bbf40f4c
lidiahasani <lidiahasani98@gmail.com>
2024-10-18 20:17:51 +0200

    Initial commit

---
+++ src/main/java/com/crispy/challenge/todoapp/TodoappApplication.java
@@ -0,0 +1,13 @@
+package com.crispy.challenge.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
