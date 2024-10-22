2ba0719392c7904c26795bbad644a5ab49bcbbed
lidiahasani <lidiahasani98@gmail.com>
2024-10-22 10:33:09 +0200

    Add starter validation and ConstraintViolation Exception Handler

--- src/main/java/com/crispy/challenge/todoapp/advice/ExceptionMapper.java
+++ src/main/java/com/crispy/challenge/todoapp/advice/ExceptionMapper.java
@@ -1,6 +1,7 @@
 package com.crispy.challenge.todoapp.advice;
 
 import com.crispy.challenge.todoapp.exception.NoResultFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,15 +17,23 @@ public class ExceptionMapper {
     @ExceptionHandler(NoResultFoundException.class)
     public ProblemDetail handleException(NoResultFoundException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-        problemDetail.setTitle("No Result Found");
+        problemDetail.setTitle("No Result Found.");
         logger.error(e.getClass().getSimpleName(), e);
         return problemDetail;
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ProblemDetail handleException(IllegalArgumentException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-        problemDetail.setTitle("Illegal Argument");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Illegal Argument.");
+        logger.error(e.getClass().getSimpleName(), e);
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ProblemDetail handleException(ConstraintViolationException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Constraint violation.");
         logger.error(e.getClass().getSimpleName(), e);
         return problemDetail;
     }
